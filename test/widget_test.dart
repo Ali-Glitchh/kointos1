0f8ec3328696e14c6f8cce64a39d5503fbc83ec9
Ali-Glitchh <syedalihaider007@gmail.com>
2025-07-25 09:39:03 +0000

    Fix test to use correct KointosApp class name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,12 +7,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:kointos/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Kointos app builds successfully', (WidgetTester tester) async {
     // This test is intentionally simple to ensure CI passes
     // In a production app, you would have meaningful widget tests here
     
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const KointosApp());
 
     // Since we don't know the exact structure, we'll just test that the app builds
     expect(find.byType(MaterialApp), findsOneWidget);
